3175d49d5a2c8b1b3f1d20c8217fcc410acfdddd
Joseph Ajileye <kunlesymls@gmail.com>
2019-11-13 20:50:54 +0100

    Adding db context to the dataaccess  project

---
+++ Blog.DataAccess/BlogDbContext.cs
@@ -0,0 +1,173 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Blog.DataAccess
+{
+    public class BlogDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            // Iterate through all EF Entity types
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                #region Convert UniqueKeyAttribute on Entities to UniqueKey in DB
+                var properties = entityType.GetProperties();
+                if (properties != null && properties.Any())
+                {
+                    foreach (var property in properties)
+                    {
+                        var uniqueKeys = GetUniqueKeyAttributes(entityType, property);
+                        if (uniqueKeys != null)
+                        {
+                            foreach (var uniqueKey in uniqueKeys.Where(x => x.Order == 0))
+                            {
+                                // Single column Unique Key
+                                if (string.IsNullOrWhiteSpace(uniqueKey.GroupId))
+                                {
+                                    entityType.AddIndex(property).IsUnique = true;
+                                }
+                                // Multiple column Unique Key
+                                else
+                                {
+                                    var mutableProperties = new List<IMutableProperty>();
+                                    properties.ToList().ForEach(x =>
+                                    {
+                                        var uks = GetUniqueKeyAttributes(entityType, x);
+                                        if (uks != null)
+                                        {
+                                            foreach (var uk in uks)
+                                            {
+                                                if (uk != null && uk.GroupId == uniqueKey.GroupId)
+                                                {
+                                                    mutableProperties.Add(x);
+                                                }
+                                            }
+                                        }
+                                    });
+                                    entityType.AddIndex(mutableProperties).IsUnique = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                #endregion Convert UniqueKeyAttribute on Entities to UniqueKey in DB
+            }
+
+            // any guid
+            string ADMIN_ID = "a18be9c0-aa65-4af8-bd17-00bd9344e575";
+            string SUPERADMIN_ID = "a18be9c0-aa65-4af8-bd17-00bd9344e675";
+            // any guid, but nothing is against to use the same one
+            string ROLE_ID = ADMIN_ID;
+            string SUPERROLE_ID = SUPERADMIN_ID;
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = ROLE_ID,
+                Name = "Admin",
+                NormalizedName = "Admin"
+            });
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = SUPERROLE_ID,
+                Name = "SuperAdmin",
+                NormalizedName = "SUPERADMIN"
+            });
+
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = ADMIN_ID,
+                UserName = "Admin@pblog.com",
+                Email = "Admin@pblog.com",
+                NormalizedUserName = "ADMIN@PBLOG.COM",
+                NormalizedEmail = "ADMIN@PBLOG.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Padmin@12345"),
+                SecurityStamp = string.Empty
+            });
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = SUPERADMIN_ID,
+                UserName = "SuperAdmin@pblog.com",
+                Email = "SuperAdmin@pblog.com",
+                NormalizedUserName = "SUPERADMIN@PBLOG.COM",
+                NormalizedEmail = "SUPERADMIN@PBLOG.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Psuperadmin@12345"),
+                SecurityStamp = string.Empty
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = ADMIN_ID
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = SUPERROLE_ID,
+                UserId = SUPERADMIN_ID
+            });
+        }
+
+        private static IEnumerable<UniqueKeyAttribute> GetUniqueKeyAttributes(IMutableEntityType entityType, IMutableProperty property)
+        {
+            if (entityType == null)
+            {
+                throw new ArgumentNullException(nameof(entityType));
+            }
+            else if (entityType.ClrType == null)
+            {
+                throw new ArgumentNullException(nameof(entityType.ClrType));
+            }
+            else if (property == null)
+            {
+                throw new ArgumentNullException(nameof(property));
+            }
+            else if (property.Name == null)
+            {
+                throw new ArgumentNullException(nameof(property.Name));
+            }
+            var propInfo = entityType.ClrType.GetProperty(
+                property.Name,
+                BindingFlags.NonPublic |
+                BindingFlags.Public |
+                BindingFlags.Static |
+                BindingFlags.Instance |
+                BindingFlags.DeclaredOnly);
+            if (propInfo == null)
+            {
+                return null;
+            }
+            return propInfo.GetCustomAttributes<UniqueKeyAttribute>();
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Blog.Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Blog.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string AppId { get; set; }
+        public string UserType { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public int NoOfLogins { get; set; }
+    }
+}
+
