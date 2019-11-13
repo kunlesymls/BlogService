54dcd1256972c2078c2cb869aada1d1f175a673a
Joseph Ajileye <kunlesymls@gmail.com>
2019-11-13 19:56:03 +0100

    Adding basic model

---
+++ Blog.Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blog.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string AppId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherName { get; set; }
+        public string Description { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Image { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ Blog.Models/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int AuthorId { get; set; }
+        public string AppId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public DateTime PostedAt { get; set; }
+        public bool IsPosted { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string AppId { get; set; }
+        public string Name { get; set; }
+    }
+}
